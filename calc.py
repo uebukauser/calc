f00d8ff881336cb059b9ddfb85c1ce71360ab1f4
uebukauser <sashatokarev2006@gmail.com>
2025-10-06 15:51:05 +0500

    calc2

--- calc.py
+++ calc.py
@@ -1,15 +1,8 @@
-def add(a, b):
-    return a + b
-
-def subtract(a, b):
-    return a - b
-def multiply(a, b):
-    return a * b
-def divide(a, b):
-    if b != 0:
-        return a / b
-    else:
-        return "Ошибка: деление на ноль"
-# Основная программа
-print("Простой калькулятор")
-print("Доступные операции: +, -, *, /")
+﻿def power(a, b):
+    return a ** b
+def square_root(a):
+    return a ** 0.5
+# Обновить основной код
+print("Улучшенный калькулятор")
+print("Доступные операции: +, -, *, /, ^, sqrt")
+bash
